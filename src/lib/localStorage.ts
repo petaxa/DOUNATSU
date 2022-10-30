39fde7ce0080da6e02945a2bfa70b5c7e3104031
tempestif <soccer.i_y@icloud.com>
2022-10-31 05:53:30 +0900

    stashing settingLogic

--- src/lib/localStorage.ts
+++ src/lib/localStorage.ts
@@ -1,11 +1,11 @@
-type localStorageKey = 'willWorkAry' | 'nextWorkedAry'
-
+type workedAryKeys = 'willWorkAry' | 'nextWorkedAry'
+type settingKeys = 'isAutocomplete' | 'isLight' | 'inputNum'
 /**
  * 任意のkeyでlocalStorageにデータをセット
  * @param key 型に登録した文字列 willWorkAry: 業務開始文言作成時の作業内容, nextWortkedAry: 日報作成時の次回業務内容
  * @param setData セットしたいデータ
  */
-export const setLocalStorage = (key: localStorageKey, setData: string[]) => {
+export const setWorkedAry = (key: workedAryKeys, setData: string[]) => {
   localStorage.setItem(key, JSON.stringify(setData))
 }
 
@@ -14,8 +14,21 @@ export const setLocalStorage = (key: localStorageKey, setData: string[]) => {
  * @param key 型に登録した文字列 willWorkAry: 業務開始文言作成時の作業内容, nextWortkedAry: 日報作成時の次回業務内容
  * @returns 登録データのオブジェクトまたはnull
  */
-export const getWorkedAry = (key: localStorageKey): string[]|null => {
+export const getWorkedAry = (key: workedAryKeys): string[]|null => {
   const storageData = localStorage.getItem(key) ?? ''
   // localStorageにデータがある場合はそのデータのオブジェクト型、ない場合はnull
   return storageData ? JSON.parse(storageData) : null
+}
+
+export const setSetting = (key: settingKeys, setData: boolean | number) => {
+  localStorage.setItem(key, String(setData))
+}
+
+export const getSetting = (key: settingKeys) => {
+  const data = localStorage.getItem(key)
+  if (key === 'isAutocomplete' || key === 'isLight') {
+    return data ? Boolean(data) : false
+  } else if (key === 'inputNum') {
+    return data ? Number(data) : 0
+  }
 }
