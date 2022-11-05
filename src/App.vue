0a4cca0b2f59099e65f427d87d9aafb8311a0d20
tempestif <soccer.i_y@icloud.com>
2022-11-05 20:11:30 +0900

    css修正

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@
 </template>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: white;
-}
+} */
 
 #nav {
   padding: 30px;
