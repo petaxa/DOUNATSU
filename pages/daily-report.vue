062db95de55800d6b8e0e98f61345edbbdeeede6
tempestif <soccer.i_y@icloud.com>
2024-05-21 04:48:57 +0900

    切り替え機構実装

--- pages/daily-report.vue
+++ pages/daily-report.vue
@@ -1,4 +1,20 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+
+const isInput = computed(() =>
+    selectedOptions.value === "作業実績" ? true : false
+);
+const selectedOptions = ref("作業実績");
+const inputOptions = ref(["作業実績", "作業予定"]);
+</script>
+
 <template>
+    <SelectButton
+        v-model="selectedOptions"
+        :options="inputOptions"
+        aria-labelledby="basic"
+    />
     <h1>【日報】</h1>
-    <dailyReportToday />
+    <dailyReportToday :isInput="isInput" />
+    <Button label="ストアのクリア" @click="resetStore" />
 </template>
